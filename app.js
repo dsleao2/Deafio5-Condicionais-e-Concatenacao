b454b4b0a7da759a6ab0fc81be5c4c575a2ba60e
Daniele Santana <dssleao2@icloud.com>
2025-03-31 18:06:47 -0300

    Desafio 5
    Criado código JS para realização do desafio usando IF e ELSE

---
+++ app.js
@@ -0,0 +1,7 @@
+let suaPontuacao = prompt ('Digite aqui a sua pontuação no jogo:');
+
+if (suaPontuacao >= 100){
+    alert('Parabéns, você venceu!');
+} else {
+    alert('Tente novamente para ganhar.');
+}
